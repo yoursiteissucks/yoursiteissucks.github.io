07ee5e61dd03ba68f184a44307d99ce6185ed2f3
Nix Siow <nixsiow@hotmail.com>
2015-06-06 01:28:28 +0800

    edit app.js file render links path

--- public/app.js
+++ public/app.js
@@ -2,31 +2,31 @@ angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mg
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/home.html',
+        templateUrl: 'public/views/home.html',
         controller: 'HomeCtrl'
       })
       .when('/about', {
-        templateUrl: 'views/about.html',
+        templateUrl: 'public/views/about.html',
         controller: ''
       })
       .when('/reviews/:id', {
-        templateUrl: 'views/detail.html',
+        templateUrl: 'public/views/detail.html',
         controller: ''
       })
       .when('/reviews', {
-        templateUrl: 'views/reviews.html',
+        templateUrl: 'public/views/reviews.html',
         controller: 'ReviewsCtrl'
       })
       .when('/signin', {
-        templateUrl: 'views/signin.html',
+        templateUrl: 'public/views/signin.html',
         controller: 'SigninCtrl'
       })
       .when('/signup', {
-        templateUrl: 'views/signup.html',
+        templateUrl: 'public/views/signup.html',
         controller: 'SignupCtrl'
       })
       .when('/add', {
-        templateUrl: 'views/add.html',
+        templateUrl: 'public/views/add.html',
         controller: 'AddCtrl'
       })
       .otherwise({
